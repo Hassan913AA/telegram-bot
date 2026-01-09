f6b24c19d402e22f1aec6b2505dfaacb5b457a15
Hassan913AA <hassanassaad913@gmail.com>
2026-01-09 21:06:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from bot.config import get_token, get_admin_id
-from bot.handlers.start import start
-from bot.handlers.broadcast import broadcast_command, handle_audio, handle_photo
-from bot.handlers.menu_handler import handle_menu
-from bot.services.user_service import load_users
-
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from config import get_token, get_admin_id
+from handlers.start import start
+from handlers.broadcast import broadcast_command, handle_audio, handle_photo
+from handlers.menu_handler import handle_menu
+from services.user_service import load_users
 
 def main():
+    # جلب التوكن ورقم الادمن من environment
     TOKEN = get_token()
     ADMIN = get_admin_id()
 
+    # إنشاء التطبيق
     app = Application.builder().token(TOKEN).build()
 
+    # إضافة بيانات البوت العامة
     app.bot_data["ADMIN"] = ADMIN
     app.bot_data["USERS"] = load_users()
 
+    # إضافة handlers
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("broadcast", broadcast_command))
-
     app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
     app.add_handler(MessageHandler(filters.AUDIO | filters.VOICE, handle_audio))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_menu))
