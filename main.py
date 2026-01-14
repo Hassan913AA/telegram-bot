d1c38b4c501a84ec1146e53568c20c46375044c3
Hassan913AA <hassanassaad913@gmail.com>
2026-01-14 13:33:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
 from config import get_token, get_admin_id
 from handlers.start import start
-from handlers.broadcast import broadcast_command, handle_audio, handle_photo
 from handlers.menu_handler import handle_menu
+from handlers.broadcast import (
+    broadcast_command,
+    handle_photo,
+    handle_audio,
+    handle_text_broadcast
+)
+
 from services.user_service import load_users
 
+
 def main():
-    # جلب التوكن ورقم الادمن من environment
     TOKEN = get_token()
     ADMIN = get_admin_id()
 
-    # عرض جزء من التوكن للتأكد من وجوده
     print("Token loaded:", TOKEN[:5], "...")
 
-    # إنشاء التطبيق
     app = Application.builder().token(TOKEN).build()
 
-    # إضافة بيانات البوت العامة
     app.bot_data["ADMIN"] = ADMIN
     app.bot_data["USERS"] = load_users()
 
-    # إضافة handlers
+    # Commands
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("broadcast", broadcast_command))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_broadcast))
+
+    # Broadcast Handlers
     app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
     app.add_handler(MessageHandler(filters.AUDIO | filters.VOICE, handle_audio))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text_broadcast))
+
+    # Menu
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_menu))
 
     print("Bot running...")
@@ -33,5 +41,4 @@ def main():
 
 
 if __name__ == "__main__":
-    print("Checking environment variables...")
     main()
