348949a129c6eb458ed9be68d2fe6b23d4ee2095
Hassan913AA <hassanassaad913@gmail.com>
2026-01-18 19:24:32 +0300

    Simplify logger setup in logger.py
    Refactored logger setup to use basicConfig for simpler logging configuration.

--- utils/logger.py
+++ utils/logger.py
@@ -1,35 +1,7 @@
-# bot/logger.py
-
 import logging
-import sys
-from pathlib import Path
 
-LOG_DIR = Path("logs")
-LOG_DIR.mkdir(exist_ok=True)
-
-LOG_FILE = LOG_DIR / "bot.log"
-
-
-def setup_logger(name: str = "bot"):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    if logger.handlers:
-        return logger
-
-    formatter = logging.Formatter(
-        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-    )
-
-    # Console handler
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
-
-    # File handler
-    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-    return logger
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
